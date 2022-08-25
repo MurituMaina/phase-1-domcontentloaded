1ef27db173f28189f5df62757ac1406d2bee7a91
MurituMaina <vincent.maina@student.moringaschool.com>
2022-08-25 07:35:22 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+//DOMContentLoaded
+//load
+
+
 // Your code goes here
+document.addEventListener("DOMContentLoaded",
+function (){
+let para = document.getElementById("text");
+    para.textContent =  "This is really cool!";
+})
+
+// function(){debugger; console.log("The DOM has loaded");})
+// console.log("tI am here already")
+
+/*defer attribute is used in the script tags
+ and prevent the js to fire up until all html page completely loads
+ */
